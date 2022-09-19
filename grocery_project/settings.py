0e69599118c079d293748612cc52dec166990694
PGA <paulgadam@gmail.com>
2022-09-19 07:20:17 -0600

    added pagination great idea from REST site

--- grocery_project/settings.py
+++ grocery_project/settings.py
@@ -51,6 +51,11 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
+
 ROOT_URLCONF = 'grocery_project.urls'
 
 TEMPLATES = [
