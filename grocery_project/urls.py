35e7aa6cade29708e0018efa8b776bf55caa608b
PGA <paulgadam@gmail.com>
2022-09-19 06:55:32 -0600

    added URL update to grocery_project

--- grocery_project/urls.py
+++ grocery_project/urls.py
@@ -18,5 +18,9 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls'))
+    path('', include('items.urls')),
+]
+
+urlpatterns += [
+    path('api-auth/', include('rest_framework.urls')),
 ]
